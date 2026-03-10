9984b5c6a1dd5ee15c75457250a7bc97454a1e9b
IlmaxRehman <ilmarehman022@gmail.com>
2026-03-10 22:25:07 +0530

    Initial PetalsStudio portfolio setup with React and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App(){
+  return(
+    <div className="bg-pink-100 h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">PetalsStudio Portfolio</h1>
+    </div>
+  )
+}
+
+export default App;
